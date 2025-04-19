2e493988e4aaa53a184fd18375c09a2bf48fe659
simonnemcic <simonnemci35@gmail.com>
2025-04-19 10:09:47 +0200

    Initial commit w/wrapper

---
+++ build/CMakeFiles/ai_term.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ai_term.dir/src/main.cpp.o"
+  "CMakeFiles/ai_term.dir/src/main.cpp.o.d"
+  "CMakeFiles/ai_term.dir/src/wrapper.cpp.o"
+  "CMakeFiles/ai_term.dir/src/wrapper.cpp.o.d"
+  "ai_term"
+  "ai_term.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ai_term.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/ai_term.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ai_term.
+# This may be replaced when dependencies are built.
