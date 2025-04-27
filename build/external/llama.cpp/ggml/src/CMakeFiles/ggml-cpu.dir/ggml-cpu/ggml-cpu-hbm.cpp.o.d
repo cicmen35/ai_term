1b6a3dab1affaa864da474fd5a0af5bdcc66a7d0
Šimon Nemčič <138312285+cicmen35@users.noreply.github.com>
2025-04-27 20:48:42 +0200

    Delete build directory

--- build/external/llama.cpp/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/ggml-cpu-hbm.cpp.o.d
+++
@@ -1,2 +0,0 @@
-external/llama.cpp/ggml/src/CMakeFiles/ggml-cpu.dir/ggml-cpu/ggml-cpu-hbm.cpp.o: \
-  /Users/simonnemcic/Desktop/Projects/ai_term/external/llama.cpp/ggml/src/ggml-cpu/ggml-cpu-hbm.cpp
